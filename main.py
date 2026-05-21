1b225f969f42a5184f384b62269b7274d70b4fee
wylliamunlimited <wylliam@sigmasquared.io>
2026-05-21 13:54:10 -0400

    Created traversal implementation to find the maximum depth of comments within a story

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ from collections import deque
 BASE_URL = 'https://hacker-news.firebaseio.com/v0'
 
 
-class Node: 
-    id: int 
-    author: str
-    kids: list
-    score: int
-    time: int
-    type: str
-
-
 def get(url: str, params: dict = {}) -> dict: 
     '''Get request helper function'''
     resp = requests.get(url, params=params)
@@ -67,10 +58,47 @@ def fetch_top_stories(pretty: bool = False) -> list:
     return resp
 
 
-def walk_thread(start_ids: list(int)): 
+def walk_thread(start_ids: list[int]): 
     '''Traverse function - walking each node and their corresponding kids
     '''
     pass
+
+
+def longest_comment(story_id: int): 
+    '''Traverse function - getting the longest comment thread
+    '''
+
+    comments = fetch_item(id=story_id).get('kids', [])
+    q = deque((kid, 1) for kid in comments)
+    max_depth = 0
+
+    while q: 
+        item_id, depth = q.popleft()
+        max_depth = max(depth, max_depth)
+
+        data = fetch_item(item_id)
+        if data:
+            for kid in data.get('kids', []):
+                q.append((kid, depth + 1))
+    return max_depth
+
+def longest_comment_dfs(story_id: int):
+
+    comments = fetch_item(id=story_id).get('kids', [])
+
+    stack = [(c, 1) for c in comments]
+    max_depth = 0
+    
+    while stack: 
+        item_id, depth = stack.pop()
+
+        max_depth = max(depth, max_depth)
+
+        item_data = fetch_item(item_id)
+        if item_data:
+            for kid in item_data.get('kids', []):
+                stack.append((kid, depth + 1))
+    return max_depth
 
 
 
@@ -85,12 +113,18 @@ def main():
     ids = fetch_top_stories()
     print(f"IDs fetched: {ids[:5]}")
     print()
+    item_data = fetch_item(ids[4])
     
-    for id in ids[:5]: 
-        print(f"Fetching the details for {id}...")
-        data = fetch_item(id)
-        print(f"data: {json.dumps(data, indent=4)}")
-        print()
+    # for id in ids[:5]: 
+    #     print(f"Fetching the details for {id}...")
+    #     data = fetch_item(id)
+    #     print(f"data: {json.dumps(data, indent=4)}")
+    #     print()
+
+    print(f"Fetching the deepest comment depth for {ids[4]} - {item_data.get('title')}...")
+    depth, depth_dfs = longest_comment(ids[4]), longest_comment_dfs(story_id=ids[4])
+    print(f"For Story {ids[4]} - Deepest Comment at depth = {depth} [BFS] vs {depth_dfs} [DFS]")
+
 
 
 if __name__ == "__main__":
